c36f7bac48eaf3f5434111cd98478e0edbb4966f
mwtas <mwtasemalkojak@gmail.com>
2021-03-12 18:49:04 -0500

    done dfs

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -40,21 +40,22 @@ class PolyTreeNode
         child_node.parent=(nil)
     end 
 
-    # def dfs(target = nil, &prc)
-    #     raise "Need a proc or target" if [target, prc].none?
-    #     prc ||= Proc.new { |node| node.value == target }
-    
-    #     return self if prc.call(self)
-    
-    #     children.each do |child|
-    #       result = child.dfs(&prc)
-    #       return result unless result.nil?
-    #     end
-    
-    #     nil
-    #   end
+   
 
-    
+    def dfs(target = nil , &prc)
+        raise "Need a proc or target" if [target,prc].none?
+        prc||= Proc.new {|node| node.value == target }
+
+        return self if prc.call(self)
+
+        children.each do |child|
+            result = child.dfs(&prc)
+            return result unless result.nil?
+
+        end 
+        nil
+    end 
+
 
 
 end
